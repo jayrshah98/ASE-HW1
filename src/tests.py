514fe866dc58180b0c7b471922dbb5b6ddeb76a0
jayrshah98 <jayrajivshah98@gmail.com>
2023-01-19 00:29:12 +0530

    Updated the tests

--- src/tests.py
+++ src/tests.py
@@ -2,6 +2,8 @@ from SYM import SYM
 from NUM import NUM
 from lib import rnd
 from main import main
+from lib import seed
+from lib import rand
 egs = {}
 def eg(key,str,func):
     egs[key] = func
@@ -20,4 +22,16 @@ def eg_num():
     for x in pairs:
         num.add(x) 
     return 11/7 == num.mid() and 0.787 == rnd(num.div())
+
+def eg_rand():
+    num1 = NUM()
+    num2 = NUM()
+    for i in range(1000):
+        num1.add(rand(0,1))
+    for i in range(1000):
+        num2.add(rand(0,1))
+    m1 = rnd(num1.mid(),10)
+    m2 = rnd(num2.mid(),10)
+    return m1==m2 and 0.5==rnd(m1,1)
+
 
