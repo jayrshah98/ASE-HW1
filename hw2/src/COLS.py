50ecd567cd0a20200be9deedbadfc0ef87eac520
Ameya Tathavadkar <ameya1398@gmail.com>
2023-01-27 03:09:23 +0530

    Changes to main.py and cols.py

--- hw2/src/COLS.py
+++ hw2/src/COLS.py
@@ -1,1 +1,20 @@
-
+class COLS:
+    def __init__(self, t, col, cols):
+        self.names = t
+        self.all = {}
+        self.x = {}
+        self.y = {}
+        self.klass = null
+    for n,s in enumerate(t):
+        col = s:find"^[A-Z]+" if NUM(n,s) else SYM(n,s)
+        self.all.append(col)
+        if not s:find"X$":
+            if s:find"!$":
+                self.klass = col
+            push(s:find"[!+-]$" if self.y else self.x, col)
+        
+    def add(self,row):
+        for _,t in self.x, self.y:
+            for _,col in t.items():
+                col.append(row.cells[col.at])
+    
