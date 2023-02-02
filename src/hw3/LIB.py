1506ac653b73237dd89ae1b72515c1478f254b59
jayrshah98 <jayrajivshah98@gmail.com>
2023-02-02 22:35:29 +0530

    updated LIB.py

---
+++ src/hw3/LIB.py
@@ -0,0 +1,75 @@
+import math
+import random
+import re
+from pathlib import Path
+
+lo = float('inf') 
+hi = float('-inf')
+seed = 937162211 
+
+class LIB:
+
+    def __init__(self):
+        pass
+
+    def rint(self, lo, hi):
+        return math.floor(0.5 + random.Random(lo,hi))
+
+    def rand(self,lo =0, hi =1):
+        global seed
+        seed = (16807 * seed) % 2147483647
+        return lo + (hi-lo) * seed / 2147483647
+
+    def rnd(self, n, nPlaces = 3):
+        mult = 10**(nPlaces)
+        return math.floor(n * mult + 0.5) / mult   
+
+    def coerce(self,s):
+        def fun(s1):
+            if s1 == "true" or s1 == "True":
+                return True
+            elif s1 == "false" or s1 == "False":
+                return False
+            else:
+                return s1
+        if type(s) == bool:
+            return s
+        try:
+            result = int(s)
+        except:
+            try:
+                result = float(s)
+            except:
+                result = fun(re.match("^\s*(.+)\s*$", s).string)
+        return result
+
+    def csv(self,sFilename,fun):
+        filePath = Path(sFilename)
+        filePath = filePath.absolute()
+        filePath = filePath.resolve()
+        f = open(filePath,"r")
+        readLines = f.readlines()
+        f.close()
+        for line in readLines:
+            t = []
+            for s1 in re.findall("([^,]+)", line):
+                t.append(self.coerce(s1))
+            fun(t)
+    
+    def kap(self, t, fun):
+        u = {}
+        for k, v in t:
+            v, k = fun(k, v)
+            u[k] = v
+        return u
+    
+    def o():
+        return
+    
+    def cosine(a,b,c):
+        x1 = (a*a + c*c - b*b) / (2*c + 0.00001)
+        x2 = max(0,min(1,x1))
+        y = (abs(a*a - x2*x2))**(0.5)
+        return (x2, y)
+
+
