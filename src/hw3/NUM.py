7fedf3cd87bcc3d2fdb53bf0a78b2787901553c8
Ameya Tathavadkar <ameya1398@gmail.com>
2023-02-02 12:16:17 -0500

    Fixed NUM.py bugs

--- src/hw3/NUM.py
+++ src/hw3/NUM.py
@@ -32,24 +32,24 @@ class NUM:
         return 0 if (self.m2 < 0 or self.n < 2) else (self.m2/(self.n-1))**0.5
     
     def rnd(self,x,n):
-        return x if x == ? else rnd(x,n)
+        return x if x == "?" else rnd(x,n)
     
     def norm(self,n):
-        return n if n == ? else (n-self.lo)/(self.hi - self.lo + math.pow(10,-32))
+        return n if n == "?" else (n-self.lo)/(self.hi - self.lo + math.pow(10,-32))
     
     def dist(self,n1,n2):
-        if n1 == ? and n2 == ?:
+        if n1 == "?" and n2 == "?":
             return 1
         n1 = self.norm(n1)
         n2 = self.norm(n2)
-        if n2 == ?:
+        if n2 == "?":
             if n1 < 0.5:
                 n2=1
             else:
                 n2=0
         
-        if n1 == ?:
-            if n2: < 0.5:
+        if n1 == "?":
+            if n2 < 0.5:
                 n1 = 1
             else:
                 n1 = 0
