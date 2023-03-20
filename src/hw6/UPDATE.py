303e253f0a1887d27e43c42b295c4dc7efe87bab
Ameya Tathavadkar <ameya1398@gmail.com>
2023-03-20 00:57:16 -0400

    Added Update to tracked files

---
+++ src/hw6/UPDATE.py
@@ -0,0 +1,57 @@
+import debugpy
+from COLS import COLS
+from LIB import LIB
+import random
+import config
+lib = LIB()
+
+rand = lib.rand
+rint = lib.rint
+
+class UPDATE:
+
+    def __init__(self) -> None:
+        pass
+
+    def add(self,col,x,n=1):
+    
+        if x != '?':
+            col.n += n
+            if hasattr(col, "isSym") and col.isSym:
+                col.has[x] = n + (col.has.get(x, 0))
+                if col.has[x] > col.most:
+                    col.most = col.has[x]
+                    col.mode = x
+            else:
+                x = float(x)
+                col.lo = min(x,col.lo)
+                col.hi = max(x,col.hi)
+                all = len(col.has)
+                if all < config.the["Max"]:
+                    pos = all + 1
+                elif random.random() < config.the["Max"] / col.n:
+                    pos = rint(1,11)
+                else:
+                    pos = None
+                if pos:
+                    if isinstance(col.has,dict):
+                        col.has[pos] = x
+                    else:
+                        col.has.append(x)
+                    col.ok = False
+
+    def adds(self, col, t):
+        for value in t or []:
+            self.add(col, value)
+        return col            
+
+
+    def row(self, data, t):
+        if data.cols:
+            data.rows.append(t)
+            for cols in [data.cols.x, data.cols.y]:
+                for col in (cols): 
+                    self.add(col.col, t[col.col.at]) 
+        else: 
+            data.cols = COLS(t)  
+        return data
